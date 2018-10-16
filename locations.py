85c23876c554a31bc3ffb11dae7cbf1905a1fb06
Kris Sakarias <ksakarias@gmail.com>
2018-10-16 15:26:25 -0700

    streamlined bot, removed redundant code

--- locations.py
+++ locations.py
@@ -1,58 +1,57 @@
 # locations.py
 # weatherbot.py imports this data and uses it to cycle through locations
 
-locations = {
-    '0' : 'pws:KWASEATT599', # First Hill
-    '1' : '/WA/Spokane',
-    '2' : '/WA/Vancouver',
-    '3' : '/WA/Yakima',
-    '4' : '/WA/Bellingham',
-    '5' : '/WA/Olympia',
-    '6' : '/WA/Pasco',
-    '7' : '/WA/Pullman',
-    '8' : '/WA/Port_Angeles',
-    '9' : '/WA/Tacoma',
-    '10' : '/WA/Bellevue',
-    '11' : '/WA/Everett',
-    '12' : '/WA/Walla_Walla',
-    '13' : '/WA/Forks',
-    '14' : '/WA/Chehalis',
-    '15' : '/WA/Ellensburg',
-    '16' : '/WA/Bremerton',
-    '17' : '/WA/Wenatchee',
-    '18' : '/WA/Leavenworth',
-    '19' : '/WA/Oak_Harbor',
-    '20' : '/WA/Friday_Harbor',
-    '21' : '/WA/Aberdeen',
-    '22' : '/WA/Anacortes',
-    '23' : '/WA/Port_Orchard',
-    '24' : '/WA/Port_Townsend',
-    '25' : '/WA/Snoqualmie',
-    '25' : '/WA/Kelso',
-    '26' : '/WA/Sequim',
-    '27' : '/WA/Poulsbo',
-    '28' : '/WA/Gig_Harbor',
-    '29' : '/WA/Ferndale',
-    '30' : '/WA/George',
-    '31' : '/WA/Marysville',
-    '32' : '/WA/Edmonds',
-    '33' : '/WA/Issaquah',
-    '34' : '/WA/Sammamish',
-    '35' : '/WA/Mount_Vernon',
-    '36' : '/WA/Lynnwood',
-    '37' : '/WA/Puyallup',
-    '38' : '/KSEA', # Seatac Airport
-    '39' : '/KBFI', # Boeing Field
-    '40' : 'pws:KWASEATT542', # Queen Anne
-    '41' : 'pws:KWASEATT351', # Magnolia
-    '42' : 'pws:KWASEATT316', # Golden Gardens Park
-    '43' : 'pws:KWASEATT1700', # Green Lake
-    '44' : 'pws:KWASEATT446', # UW
-    '45' : 'pws:KWASEATT1303', # Downtown
-    '46' : 'pws:KWASEATT169', # Capitol Hill
-    '47' : '/WA/Skykomish', # Stevens Pass
-    '48' : 'Snoqualmie_pass', # Snoqualmie Pass
-    '49' : 'pws:KWADEMIN14', # Mt. Baker
-    '50' : 'pws:KWABUCKL19' # Mt. Rainier Paradise Inn
-
-}
+locations = [
+  'pws:KWASEATT599', # First Hill
+  '/WA/Spokane',
+  '/WA/Vancouver',
+  '/WA/Yakima',
+  '/WA/Bellingham',
+  '/WA/Olympia',
+  '/WA/Pasco',
+  '/WA/Pullman',
+  '/WA/Port_Angeles',
+  '/WA/Tacoma',
+  '/WA/Bellevue',
+  '/WA/Everett',
+  '/WA/Walla_Walla',
+  '/WA/Forks',
+  '/WA/Chehalis',
+  '/WA/Ellensburg',
+  '/WA/Bremerton',
+  '/WA/Wenatchee',
+  '/WA/Leavenworth',
+  '/WA/Oak_Harbor',
+  '/WA/Friday_Harbor',
+  '/WA/Aberdeen',
+  '/WA/Anacortes',
+  '/WA/Port_Orchard',
+  '/WA/Port_Townsend',
+  '/WA/Snoqualmie',
+  '/WA/Kelso',
+  '/WA/Sequim',
+  '/WA/Poulsbo',
+  '/WA/Gig_Harbor',
+  '/WA/Ferndale',
+  '/WA/George',
+  '/WA/Marysville',
+  '/WA/Edmonds',
+  '/WA/Issaquah',
+  '/WA/Sammamish',
+  '/WA/Mount_Vernon',
+  '/WA/Lynnwood',
+  '/WA/Puyallup',
+  '/KSEA', # Seatac Airport
+  '/KBFI', # Boeing Field
+  'pws:KWASEATT542', # Queen Anne
+  'pws:KWASEATT351', # Magnolia
+  'pws:KWASEATT316', # Golden Gardens Park
+  'pws:KWASEATT1700', # Green Lake
+  'pws:KWASEATT446', # UW
+  'pws:KWASEATT1303', # Downtown
+  'pws:KWASEATT169', # Capitol Hill
+  '/WA/Skykomish', # Stevens Pass
+  'Snoqualmie_pass', # Snoqualmie Pass
+  'pws:KWADEMIN14', # Mt. Baker
+  'pws:KWABUCKL19' # Mt. Rainier Paradise Inn
+]
--- weatherbot.py
+++ weatherbot.py
@@ -2,172 +2,72 @@
 # A twitter bot that interacts with the weather underground and twitter apis to tweet regularly about weather conditions from various locations around
 # Washington State.
 
-import os, tweepy, random, time
-
+import os, tweepy, random, time, requests
 from dotenv import load_dotenv
 from json import loads
-from urllib.request import urlopen
 from locations import locations
 
 load_dotenv()
 
-
 # these methods fetch json data from the wunderground api
 def get_current_data(location):
-    current_url = 'http://api.wunderground.com/api/' + classifiedInfo.weather_key + '/conditions/q/' + location + '.json'
-    current_req = urlopen(current_url)
-    current_data = loads(current_req.read().decode('utf-8'))
-    return current_data
+  url = 'http://api.wunderground.com/api/' + os.getenv('WU_API_KEY') + '/conditions/q/' + location + '.json'
+  res = requests.get(url)
+  res.raise_for_status()
+  return res.json()
 
 def get_forecast(location):
-    forecast_url = 'http://api.wunderground.com/api/' + classifiedInfo.weather_key + '/forecast/q/' + location + '.json'
-    forecast_req = urlopen(forecast_url)
-    forecast_data = loads(forecast_req.read().decode('utf-8'))
-    return forecast_data
+  url = 'http://api.wunderground.com/api/' + os.getenv('WU_API_KEY') + '/forecast/q/' + location + '.json'
+  res = requests.get(url)
+  res.raise_for_status()
+  return res.json()
 
 
-cities = []
-# loop generates random numbers and determines locations to tweet about based on the random numbers
-for x in range(0, int(round(len(locations)/10))):
-    rand = random.randint(0, len(locations))
+# generates random numbers and determines locations to tweet about based on the random numbers
+def getCities():
+  cities = []
+  for x in range(0, int(round(len(locations) / 10))):
+    rand = random.randint(0, len(locations) - 1)
+    city = locations[rand]
     
-    if rand == 0:
-        city = locations['0']
-    elif rand == 1:
-        city = locations['1']
-    elif rand == 2:
-        city = locations['2']
-    elif rand == 3:
-        city = locations['3']
-    elif rand == 4:
-        city = locations['4']
-    elif rand == 5:
-        city = locations['5']
-    elif rand == 6:
-        city = locations['6']
-    elif rand == 7:
-        city = locations['7']
-    elif rand == 8:
-        city = locations['8']
-    elif rand == 9:
-        city = locations['9']
-    elif rand == 10:
-        city = locations['10']
-    elif rand == 11:
-        city = locations['11']
-    elif rand == 12:
-        city = locations['12']
-    elif rand == 13:
-        city = locations['13']
-    elif rand == 14:
-        city = locations['14']
-    elif rand == 15:
-        city = locations['15']
-    elif rand == 16:
-        city = locations['16']
-    elif rand == 17:
-        city = locations['17']
-    elif rand == 18:
-        city = locations['18']
-    elif rand == 19:
-        city = locations['19']
-    elif rand == 20:
-        city = locations['20']
-    elif rand == 21:
-        city = locations['21']
-    elif rand == 22:
-        city = locations['22']
-    elif rand == 23:
-        city = locations['23']
-    elif rand == 24:
-        city = locations['24']
-    elif rand == 25:
-        city = locations['25']
-    elif rand == 26:
-        city = locations['26']
-    elif rand == 27:
-        city = locations['27']
-    elif rand == 28:
-        city = locations['28']
-    elif rand == 29:
-        city = locations['29']
-    elif rand == 30:
-        city = locations['30']
-    elif rand == 31:
-        city = locations['31']
-    elif rand == 32:
-        city = locations['32']
-    elif rand == 33:
-        city = locations['33']
-    elif rand == 34:
-        city = locations['34']
-    elif rand == 35:
-        city = locations['35']
-    elif rand == 36:
-        city = locations['36']
-    elif rand == 37:
-        city = locations['37']
-    elif rand == 38:
-        city = locations['38']
-    elif rand == 39:
-        city = locations['39']
-    elif rand == 40:
-        city = locations['40']
-    elif rand == 41:
-        city = locations['41']
-    elif rand == 42:
-        city = locations['42']
-    elif rand == 43:
-        city = locations['43']
-    elif rand == 44:
-        city = locations['44']
-    elif rand == 45:
-        city = locations['45']
-    elif rand == 46:
-        city = locations['46']
-    elif rand == 47:
-        city = locations['47']
-    elif rand == 48:
-        city = locations['48']
-    elif rand == 49:
-        city = locations['49']
-    else:
-        city = locations['50']
-
     #prevents duplicate status updates for the same location
     if city not in cities:
-        cities.append(city)
-
-    x+=1
+      cities.append(city)
+  return cities
 
 
 # connect with twitter api, loop through tweet locations in list generated above and create a status for each
 # post the status, wait a given time until posting the next tweet
 if __name__ == '__main__':
-    auth = tweepy.OAuthHandler(classifiedInfo.consumer_key, classifiedInfo.consumer_secret)
-    auth.set_access_token(classifiedInfo.access_token, classifiedInfo.access_secret)
-    api = tweepy.API(auth)
+  auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
+  auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_SECRET'))
+  api = tweepy.API(auth)
+  
+  cities = getCities()
+
+  for city in cities:
+    time.sleep(10)
+    current_data = get_current_data(city)
+    forecast_data = get_forecast(city)
+
+    current_conditions = current_data['current_observation']['weather']
+    current_temp = current_data['current_observation']['temperature_string']
+    current_humidity = current_data['current_observation']['relative_humidity']
+    forecast = forecast_data['forecast']['txt_forecast']['forecastday'][0]['fcttext']
+    forecast_url = current_data['current_observation']['forecast_url']
+  
+    if city.startswith('pws'):
+      location = current_data['current_observation']['observation_location']['city']
+    else:
+      location = current_data['current_observation']['display_location']['city']
+
+
+    status = '%s: Currently %s with a temperature of %s, and a relative humidity of %s. Today\'s forecast: %s %s' % (location, current_conditions, current_temp, current_humidity, forecast, forecast_url)
+
+    try:
+      api.update_status(status)
+    except tweepy.TweepError:
+      pass
     
-    for city in cities:
-        time.sleep(20)
-        current_data = get_current_data(city)
-        forecast_data = get_forecast(city)
-        
-        if city.startswith('pws'):
-            status = (current_data['current_observation']['observation_location']['city'] + ': Currently ' + current_data['current_observation']['weather'] + ' with a temperature of ' +
-                      current_data['current_observation']['temperature_string'] + ', and a relative humidity of ' +
-                      current_data['current_observation']['relative_humidity'] +
-                      '. Today\'s forecast: ' + forecast_data['forecast']['txt_forecast']['forecastday'][0]['fcttext'] + ' ' + current_data['current_observation']['forecast_url'])
-        else:
-            status = (current_data['current_observation']['display_location']['city'] + ': Currently ' + current_data['current_observation']['weather'] + ' with a temperature of ' +
-                  current_data['current_observation']['temperature_string'] + ', and a relative humidity of ' +
-                  current_data['current_observation']['relative_humidity'] +
-                  '. Today\'s forecast: ' + forecast_data['forecast']['txt_forecast']['forecastday'][0]['fcttext'] + ' ' + current_data['current_observation']['forecast_url'])
-    
-        try:
-            api.update_status(status)
-        except tweepy.TweepError:
-            pass
-        
-        print(status)
+    print(status)
 
