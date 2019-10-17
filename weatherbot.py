e0619830edfbd6e6f05e5da7e3b2d6b821a4eacd
Kris Sakarias <ksakarias@gmail.com>
2019-10-17 16:09:12 -0700

    fix package problem locally that prevented tweets from sending; clean up some names

--- weatherbot.py
+++ weatherbot.py
@@ -2,7 +2,7 @@
 # A twitter bot that interacts with the weather underground and twitter apis to tweet regularly about weather conditions from various locations around
 # Washington State.
 
-import os, tweepy, random, time, requests
+import os, twitter, random, time, requests
 from dotenv import load_dotenv
 from json import loads
 from locations import locations
@@ -10,23 +10,23 @@ from locations import locations
 load_dotenv()
 
 # fetch json data from the weather.gov api
-def get_forecast(location):
-  url = 'https://api.weather.gov/points/' + location
+def get_forecast(cityCoordinates):
+  url = 'https://api.weather.gov/points/' + cityCoordinates
   res = requests.get(url)
   res.raise_for_status()
   json = res.json()
   props = json['properties']['forecast']
-  loc = json['properties']['relativeLocation']['properties']['city']
-  resFore = requests.get(props)
-  resFore.raise_for_status()
-  return { 'city': loc, 'data': resFore.json() }
+  location = json['properties']['relativeLocation']['properties']['city']
+  resForecast = requests.get(props)
+  resForecast.raise_for_status()
+  return { 'city': location, 'data': resForecast.json() }
 
 
 # generates random numbers and determines locations to tweet about based on the random numbers
 def getCities():
   locales = list(locations.keys())
   cities = []
-  for x in range(0, 5):
+  for i in range(0, 5):
     rand = random.randint(0, len(locales) - 1)
     city = locales[rand]
     
@@ -39,32 +39,31 @@ def getCities():
 # connect with twitter api, loop through tweet locations in list generated above and create a status for each
 # post tweet, wait a given time until posting the next tweet
 if __name__ == '__main__':
-  auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
-  auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_SECRET'))
-  api = tweepy.API(auth)
+  twitterConnection = twitter.Api(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'), os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_SECRET'))
   
   cities = getCities()
   print(cities)
 
   for city in cities:
-    coords = locations[city]
+    cityCoordinates = locations[city]
     time.sleep(2)
-    info = get_forecast(coords)
-    forecast = info['data']
-    locale = info['city']
+    forecastRes = get_forecast(cityCoordinates)
+    forecast = forecastRes['data']
+    locale = forecastRes['city']
 
-    current = forecast['properties']['periods'][0]
+    currentConditions = forecast['properties']['periods'][0]
     forecast = forecast['properties']['periods'][1]
 
-    statusFull = '%s: %sF - %s' % (city, current['temperature'], current['detailedForecast'])
-    statusShort = '%s: %sF - %s %s: %s.' % (city, current['temperature'], current['shortForecast'], forecast['name'], forecast['shortForecast'])
+    statusFull = '%s: %sF - %s' % (city, currentConditions['temperature'], currentConditions['detailedForecast'])
+    statusShort = '%s: %sF - %s %s: %s.' % (city, currentConditions['temperature'], currentConditions['shortForecast'], forecast['name'], forecast['shortForecast'])
 
     try:
       if len(statusFull) <= 280:
         print(statusFull)
-        api.update_status(statusFull)
+        twitterConnection.PostUpdate(statusFull)
       else:
         print(statusShort)
-        api.update_status(statusShort)
-    except tweepy.TweepError:
-      pass
+        twitterConnection.PostUpdate(statusShort)
+    except twitter.error.TwitterError:
+      print('Error - Tweet unable to post')
+
